70f8d2d40d68c59b79ee9f494c2e70a4a6796fb1
bpmallen <bpmallen@gmx.com>
2025-02-11 20:52:32 +0000

    1.1 - auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({ extended: true })); // to parse form data (urlencoded)
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
