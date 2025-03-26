6640beab81221c3be0d595e464cf0c51399a1515
bpmallen <bpmallen@gmx.com>
2025-03-26 12:37:36 +0000

    2.4 - UI - Header Skeleton edit

--- frontend/src/components/skeletons/ProfileHeaderSkeleton.jsx
+++ frontend/src/components/skeletons/ProfileHeaderSkeleton.jsx
@@ -4,15 +4,15 @@ const ProfileHeaderSkeleton = () => {
       <div className="flex gap-2 items-center">
         <div className="flex flex-1 gap-1">
           <div className="flex flex-col gap-1 w-full">
-            <div className="skeleton h-4 w-12 rounded-full"></div>
-            <div className="skeleton h-4 w-16 rounded-full"></div>
+            <div className="skeleton h-4 w-12 rounded-full [animation-duration:1500ms]"></div>
+            <div className="skeleton h-4 w-16 rounded-full [animation-duration:1500ms]"></div>
             <div className="skeleton h-40 w-full relative">
-              <div className="skeleton h-20 w-20 rounded-full border absolute -bottom-10 left-3"></div>
+              <div className="skeleton h-20 w-20 rounded-full border absolute -bottom-10 left-3 [animation-duration:1500ms]"></div>
             </div>
-            <div className="skeleton h-6 mt-4 w-24 ml-auto rounded-full"></div>
-            <div className="skeleton h-4 w-14 rounded-full mt-4"></div>
-            <div className="skeleton h-4 w-20 rounded-full"></div>
-            <div className="skeleton h-4 w-2/3 rounded-full"></div>
+            <div className="skeleton h-6 mt-4 w-24 ml-auto rounded-full [animation-duration:1500ms]"></div>
+            <div className="skeleton h-4 w-14 rounded-full mt-4 [animation-duration:1500ms]"></div>
+            <div className="skeleton h-4 w-20 rounded-full [animation-duration:1500ms]"></div>
+            <div className="skeleton h-4 w-2/3 rounded-full [animation-duration:1500ms]"></div>
           </div>
         </div>
       </div>
