fdcbc3d7888609eb815963e960d5cb7b04b01497
bpmallen <bpmallen@gmx.com>
2025-03-29 09:43:48 +0000

    3.6 - fetch posts for the HomePage

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -18,18 +18,14 @@ const HomePage = () => {
             onClick={() => setFeedType("forYou")}
           >
             For you
-            {feedType === "forYou" && (
-              <div className="absolute bottom-0 w-10  h-1 rounded-full bg-primary"></div>
-            )}
+            {feedType === "forYou" && <div className="absolute bottom-0 w-10  h-1 rounded-full bg-primary"></div>}
           </div>
           <div
             className="flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
             onClick={() => setFeedType("following")}
           >
             Following
-            {feedType === "following" && (
-              <div className="absolute bottom-0 w-10  h-1 rounded-full bg-primary"></div>
-            )}
+            {feedType === "following" && <div className="absolute bottom-0 w-10  h-1 rounded-full bg-primary"></div>}
           </div>
         </div>
 
@@ -37,7 +33,7 @@ const HomePage = () => {
         <CreatePost />
 
         {/* POSTS */}
-        <Posts />
+        <Posts feedType={feedType} />
       </div>
     </>
   );
