474ff17697ca2c8165f55cbe090cd39b962bb1d8
bpmallen <bpmallen@gmx.com>
2025-03-30 08:26:28 +0100

    3.7 - handle delete posts

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -5,19 +5,50 @@ import { FaRegBookmark } from "react-icons/fa6";
 import { FaTrash } from "react-icons/fa";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+import LoadingSpinner from "./LoadingSpinner";
 
 const Post = ({ post }) => {
   const [comment, setComment] = useState("");
+  const { data: authUser } = useQuery({ queryKey: ["authUser"] });
+  const queryClient = useQueryClient();
+
+  const { mutate: deletePost, isPending } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch(`/api/posts/${post._id}`, {
+          method: "DELETE",
+        });
+        const data = await res.json();
+
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: () => {
+      toast.success("Post deleted successfully");
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
+    },
+  });
+
   const postOwner = post.user;
   const isLiked = false;
 
-  const isMyPost = true;
+  const isMyPost = authUser._id === post.user._id;
 
   const formattedDate = "1h";
 
   const isCommenting = false;
 
-  const handleDeletePost = () => {};
+  const handleDeletePost = () => {
+    deletePost();
+  };
 
   const handlePostComment = (e) => {
     e.preventDefault();
@@ -29,10 +60,7 @@ const Post = ({ post }) => {
     <>
       <div className="flex gap-2 items-start p-4 border-b border-gray-700">
         <div className="avatar">
-          <Link
-            to={`/profile/${postOwner.username}`}
-            className="w-8 rounded-full overflow-hidden"
-          >
+          <Link to={`/profile/${postOwner.username}`} className="w-8 rounded-full overflow-hidden">
             <img src={postOwner.profileImg || "/avatar-placeholder.png"} />
           </Link>
         </div>
@@ -42,79 +70,51 @@ const Post = ({ post }) => {
               {postOwner.fullName}
             </Link>
             <span className="text-gray-700 flex gap-1 text-sm">
-              <Link to={`/profile/${postOwner.username}`}>
-                @{postOwner.username}
-              </Link>
+              <Link to={`/profile/${postOwner.username}`}>@{postOwner.username}</Link>
               <span>·</span>
               <span>{formattedDate}</span>
             </span>
             {isMyPost && (
               <span className="flex justify-end flex-1">
-                <FaTrash
-                  className="cursor-pointer hover:text-red-500"
-                  onClick={handleDeletePost}
-                />
+                {!isPending && <FaTrash className="cursor-pointer hover:text-red-500" onClick={handleDeletePost} />}
+                {isPending && <LoadingSpinner size="sm" />}
               </span>
             )}
           </div>
           <div className="flex flex-col gap-3 overflow-hidden">
             <span>{post.text}</span>
             {post.img && (
-              <img
-                src={post.img}
-                className="h-80 object-contain rounded-lg border border-gray-700"
-                alt=""
-              />
+              <img src={post.img} className="h-80 object-contain rounded-lg border border-gray-700" alt="" />
             )}
           </div>
           <div className="flex justify-between mt-3">
             <div className="flex gap-4 items-center w-2/3 justify-between">
               <div
                 className="flex gap-1 items-center cursor-pointer group"
-                onClick={() =>
-                  document
-                    .getElementById("comments_modal" + post._id)
-                    .showModal()
-                }
+                onClick={() => document.getElementById("comments_modal" + post._id).showModal()}
               >
                 <FaRegComment className="w-4 h-4  text-slate-500 group-hover:text-sky-400" />
-                <span className="text-sm text-slate-500 group-hover:text-sky-400">
-                  {post.comments.length}
-                </span>
+                <span className="text-sm text-slate-500 group-hover:text-sky-400">{post.comments.length}</span>
               </div>
               {/* We're using Modal Component from DaisyUI */}
-              <dialog
-                id={`comments_modal${post._id}`}
-                className="modal border-none outline-none"
-              >
+              <dialog id={`comments_modal${post._id}`} className="modal border-none outline-none">
                 <div className="modal-box rounded border border-gray-600">
                   <h3 className="font-bold text-lg mb-4">COMMENTS</h3>
                   <div className="flex flex-col gap-3 max-h-60 overflow-auto">
                     {post.comments.length === 0 && (
-                      <p className="text-sm text-slate-500">
-                        No comments yet 🤔 Be the first one 😉
-                      </p>
+                      <p className="text-sm text-slate-500">No comments yet 🤔 Be the first one 😉</p>
                     )}
                     {post.comments.map((comment) => (
                       <div key={comment._id} className="flex gap-2 items-start">
                         <div className="avatar">
                           <div className="w-8 rounded-full">
-                            <img
-                              src={
-                                comment.user.profileImg ||
-                                "/avatar-placeholder.png"
-                              }
-                            />
+                            <img src={comment.user.profileImg || "/avatar-placeholder.png"} />
                           </div>
                         </div>
                         <div className="flex flex-col">
                           <div className="flex items-center gap-1">
-                            <span className="font-bold">
-                              {comment.user.fullName}
-                            </span>
-                            <span className="text-gray-700 text-sm">
-                              @{comment.user.username}
-                            </span>
+                            <span className="font-bold">{comment.user.fullName}</span>
+                            <span className="text-gray-700 text-sm">@{comment.user.username}</span>
                           </div>
                           <div className="text-sm">{comment.text}</div>
                         </div>
@@ -132,11 +132,7 @@ const Post = ({ post }) => {
                       onChange={(e) => setComment(e.target.value)}
                     />
                     <button className="btn btn-primary rounded-full btn-sm text-white px-4">
-                      {isCommenting ? (
-                        <span className="loading loading-spinner loading-md"></span>
-                      ) : (
-                        "Post"
-                      )}
+                      {isCommenting ? <span className="loading loading-spinner loading-md"></span> : "Post"}
                     </button>
                   </form>
                 </div>
@@ -146,26 +142,13 @@ const Post = ({ post }) => {
               </dialog>
               <div className="flex gap-1 items-center group cursor-pointer">
                 <BiRepost className="w-6 h-6  text-slate-500 group-hover:text-green-500" />
-                <span className="text-sm text-slate-500 group-hover:text-green-500">
-                  0
-                </span>
+                <span className="text-sm text-slate-500 group-hover:text-green-500">0</span>
               </div>
-              <div
-                className="flex gap-1 items-center group cursor-pointer"
-                onClick={handleLikePost}
-              >
-                {!isLiked && (
-                  <FaRegHeart className="w-4 h-4 cursor-pointer text-slate-500 group-hover:text-pink-500" />
-                )}
-                {isLiked && (
-                  <FaRegHeart className="w-4 h-4 cursor-pointer text-pink-500 " />
-                )}
+              <div className="flex gap-1 items-center group cursor-pointer" onClick={handleLikePost}>
+                {!isLiked && <FaRegHeart className="w-4 h-4 cursor-pointer text-slate-500 group-hover:text-pink-500" />}
+                {isLiked && <FaRegHeart className="w-4 h-4 cursor-pointer text-pink-500 " />}
 
-                <span
-                  className={`text-sm text-slate-500 group-hover:text-pink-500 ${
-                    isLiked ? "text-pink-500" : ""
-                  }`}
-                >
+                <span className={`text-sm text-slate-500 group-hover:text-pink-500 ${isLiked ? "text-pink-500" : ""}`}>
                   {post.likes.length}
                 </span>
               </div>
